fb809b2df5a42321d4f8e2d6fe2d9efbd7e7c3a2
Brad Davidson <brad.davidson@rancher.com>
2021-06-12 03:10:08 -0700

    Set deployed timestamp on change
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/status/status.go
+++ pkg/status/status.go
@@ -3,6 +3,7 @@ package status
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/brandond/helm-set-status/pkg/common"
 	"github.com/pkg/errors"
@@ -23,9 +24,10 @@ func SetStatus(options common.RunOptions) error {
 		return errors.Wrapf(err, "failed to get release %s", options.ReleaseName)
 	}
 	latestRelease.SetStatus(options.ReleaseStatus, fmt.Sprintf("status forced by %s", os.Args[0]))
+	latestRelease.Info.LastDeployed.Time = time.Now().UTC()
 
 	if err := recordRelease(latestRelease, cfg); err != nil {
-		return errors.Wrapf(err, "failed to update release %s", options.ReleaseName, options.ReleaseNamespace)
+		return errors.Wrapf(err, "failed to update release %s", options.ReleaseName)
 	}
 	return nil
 }
